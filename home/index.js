fe2b22966c46bc98c07f55f19465987e93518c55
Arsenii Hoi <origingoy@gmail.com>
2023-06-19 18:29:16 +0300

    last commit for this repo, else with django

---
+++ home/index.js
@@ -0,0 +1,43 @@
+const numberOfImages = 3;
+const prevButton = document.querySelector(".part-6 .prev-btn");
+const nextButton = document.querySelector(".part-6 .next-btn");
+
+function getImageElement() {
+    return document.querySelector(".part-6 .review-img");
+}
+
+function getCurrentActiveIndex() {
+    const elem = getImageElement();
+    const index = elem.getAttribute("src").match(/\d/);
+    return Number.parseInt(index);
+}
+
+function setNewIndex(index) {
+    const elem = getImageElement();
+    elem.setAttribute("src", `images/reviews/rev${index}.jpg`)
+}
+
+function prevBtnTrigger() {
+    const oldIndex = getCurrentActiveIndex();
+    let newIndex = oldIndex - 1;
+
+    if (newIndex < 0) {
+        newIndex = numberOfImages - 1;
+    }
+
+    setNewIndex(newIndex);
+}
+
+function nextBtnTrigger() {
+    const oldIndex = getCurrentActiveIndex();
+    let newIndex = oldIndex + 1;
+
+    if (newIndex >= numberOfImages) {
+        newIndex = 0;
+    }
+
+    setNewIndex(newIndex);
+}
+
+prevButton.addEventListener("click", prevBtnTrigger)
+nextButton.addEventListener("click", nextBtnTrigger)
